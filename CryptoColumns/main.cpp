8a9f287fbbddc9904dcc573545b811481531dc87
ekuri <wuwy23@mail2.sysu.edu.cn>
2015-10-20 16:53:50 +0800

    delete some build directory and add CryptoColumns project

---
+++ CryptoColumns/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string keywords;
+    string cipherText;
+    while (true) {
+        cin >> keywords;
+        if (keywords == "THEEND")
+            break;
+
+        cin >> keywords;
+    }
+    return 0;
+}
+
